a966734676bad61ac59d137948d0d42872e65ef9
ppsobralwb <ppsobralwb@gmail.com>
2023-02-16 10:37:07 -0300

    Error FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,37 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
+        })
     ],
+
+    jwt: {
+        signingKey: process.env.SIGNING_KEY,
+    },
+    
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            console.log(email)
+
+            try {
+                await fauna.query(
+                    q.Create(
+                        q.Collection('users'),
+                        { data: { email } }
+                    )
+                )
+                return true
+            } catch {
+                return false
+            }
+        },
+    }
 })
